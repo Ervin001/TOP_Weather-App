75439b4f4c2e628fcd7d0a587ed0a8fbae51fc9f
Ervin <evgnz24@gmail.com>
2023-02-24 23:20:17 -0600

    classes are now in one file, and there is one function in each class.

--- src/logic.js
+++ src/logic.js
@@ -1,18 +1,19 @@
-class Logic {
-  constructor(api_key) {
-    this.#apiKey = api_key;
-  }
+// class Logic {
+//   #apiKey;
+//   constructor(api_key) {
+//     this.#apiKey = api_key;
+//   }
 
-  getData = async () => {
-    try {
-      // call api
-      const data = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${
-          this.#apiKey
-        }`
-      );
-      // get json object of city
-      const cityData = await data.json();
-    } catch (err) {}
-  };
-}
+//   getData = async () => {
+//     try {
+//       // call api
+//       const data = await fetch(
+//         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${
+//           this.#apiKey
+//         }`
+//       );
+//       // get json object of city
+//       const cityData = await data.json();
+//     } catch (err) {}
+//   };
+// }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,64 @@
+// eslint-disable-next-line max-classes-per-file
 import { API_KEY } from './config.js';
-import DisplayUI from './display.js';
-import Logic from './logic.js';
+// import DisplayUI from './display.js';
+// import Logic from './logic.js';
+
+class Logic {
+  #apiKey;
+
+  #tempFormat = 'fahrenheit';
+
+  constructor(api_key) {
+    this.#apiKey = api_key;
+  }
+
+  getData = async (city) => {
+    try {
+      // call api
+      const data = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${
+          this.#apiKey
+        }`
+      );
+      // get json object of city
+      const cityData = await data.json();
+      // eslint-disable-next-line no-use-before-define
+      display.updateInfo(city, cityData);
+    } catch (err) {}
+  };
+}
+
+class DisplayUI {
+  updateInfo(city, data) {
+    let { feels_like, humidity, pressure, temp, temp_max, temp_min } =
+      data.main;
+
+    temp = Math.round(temp);
+
+    this.logInfo(
+      feels_like,
+      humidity,
+      pressure,
+      temp,
+      temp_max,
+      temp_min,
+      city
+    );
+    const tempEl = document.querySelector('.temperature');
+    const dateEl = document.querySelector('.date');
+    const locationEl = document.querySelector('.location');
+
+    // set temp
+    tempEl.textContent = temp;
+    // set city
+    locationEl.textContent = city;
+    // set date
+  }
+
+  logInfo(...data) {
+    console.log(`%c ${data}`, 'color: red;');
+  }
+}
 
 const logic = new Logic(API_KEY);
 const display = new DisplayUI();
@@ -9,12 +67,12 @@ const searchField = document.querySelector('.search');
 
 searchField.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
-    callApi(searchField.value);
+    logic.getData(searchField.value);
   }
 });
 
 function init() {
-  callApi('Dallas');
+  logic.getData('new york');
 }
 
 init();

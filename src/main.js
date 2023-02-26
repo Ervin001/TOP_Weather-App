61d89cc77327ab03023c1f8889cae312b6b317b7
Ervin <evgnz24@gmail.com>
2023-02-25 19:19:41 -0600

    Refactoring logic and weather class to implement SOLID principles

--- src/main.js
+++ src/main.js
@@ -3,10 +3,21 @@ import { API_KEY } from './config.js';
 // import DisplayUI from './display.js';
 // import Logic from './logic.js';
 
+const tempEl = document.querySelector('.temperature');
+
 class Logic {
   #apiKey;
 
-  #tempFormat = 'fahrenheit';
+  #weatherData = {
+    feelsLike: undefined,
+    humidity: undefined,
+    pressure: undefined,
+    temp: undefined,
+    tempMax: undefined,
+    tempMin: undefined,
+  };
+
+  celsius = false;
 
   constructor(api_key) {
     this.#apiKey = api_key;
@@ -22,41 +33,67 @@ class Logic {
       );
       // get json object of city
       const cityData = await data.json();
+      const { feels_like, humidity, pressure, temp, temp_max, temp_min } =
+        cityData.main;
+
+      this.#weatherData.feelsLike = feels_like;
+      this.#weatherData.humidity = humidity;
+      this.#weatherData.pressure = pressure;
+      this.#weatherData.temp = temp;
+      this.#weatherData.tempMax = temp_max;
+      this.#weatherData.tempMin = temp_min;
+
       // eslint-disable-next-line no-use-before-define
-      display.updateInfo(city, cityData);
-    } catch (err) {}
+      display.updateInfo(city, this.#weatherData);
+    } catch (err) {
+      console.log(err);
+    }
   };
+
+  toggleFormat() {
+    if (!this.celsius) return;
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const property in this.#weatherData) {
+      if (property !== 'humidity' && property !== 'pressure') {
+        this.#weatherData[property] = Math.round(
+          ((this.#weatherData[property] - 32) * 5) / 9
+        );
+      }
+    }
+  }
+
+  toggleCelsius() {
+    return this.celsius === false
+      ? (this.celsius = true)
+      : (this.celsius = false);
+  }
+  logData() {
+    console.log(this.#weatherData);
+  }
 }
 
 class DisplayUI {
-  updateInfo(city, data) {
-    let { feels_like, humidity, pressure, temp, temp_max, temp_min } =
-      data.main;
-
-    temp = Math.round(temp);
-
-    this.logInfo(
-      feels_like,
-      humidity,
-      pressure,
-      temp,
-      temp_max,
-      temp_min,
-      city
-    );
-    const tempEl = document.querySelector('.temperature');
-    const dateEl = document.querySelector('.date');
+  updateInfo(city, dataObj) {
+    const { feelsLike, humidity, pressure, temp, tempMax, tempMin } = dataObj;
+    const tempEl = document.querySelector('.temp-number');
     const locationEl = document.querySelector('.location');
 
-    // set temp
-    tempEl.textContent = temp;
-    // set city
-    locationEl.textContent = city;
-    // set date
+    // First Word in city to upper case
+    const upperCity = city.charAt(0).toUpperCase() + city.slice(1);
+
+    tempEl.textContent = Math.round(temp);
+    locationEl.textContent = upperCity;
+    this.changeFormat();
+    logic.toggleFormat();
+    logic.logData();
   }
 
-  logInfo(...data) {
-    console.log(`%c ${data}`, 'color: red;');
+  changeFormat() {
+    const tempEl = document.querySelector('.temp-number');
+    tempEl.textContent = Math.round(((+tempEl.textContent - 32) * 5) / 9);
+
+    console.log(tempEl.textContent);
   }
 }
 
@@ -71,8 +108,11 @@ searchField.addEventListener('keypress', (e) => {
   }
 });
 
+tempEl.addEventListener('click', (e) => {});
+
 function init() {
-  logic.getData('new york');
+  // logic.getData('new york');
+  logic.getData('austin');
 }
 
 init();
